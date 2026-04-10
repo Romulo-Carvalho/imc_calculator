c13baca60d944bb223f54611ae5943537004137d
Romulo <carvalhoreisr@gmail.com>
2026-04-10 17:36:20 -0300

    male and female icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const containerBC = Color(0xff1d1e33);
@@ -22,17 +23,39 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: ReusableCard(colour: containerBC)),
-                  Expanded(child: ReusableCard(colour: containerBC)),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: containerBC,
+                      cardChild: IconContent(icon: FontAwesomeIcons.mars, text: 'MALE'),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: containerBC,
+                      cardChild: IconContent(icon: FontAwesomeIcons.venus, text: 'FEMALE'),
+                    ),
+                  ),
                 ],
               ),
             ),
-            Expanded(child: ReusableCard(colour: containerBC)),
+            Expanded(
+              child: ReusableCard(
+                colour: containerBC,
+              ),
+            ),
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: ReusableCard(colour: containerBC)),
-                  Expanded(child: ReusableCard(colour: containerBC)),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: containerBC,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: containerBC,
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -50,9 +73,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({super.key, required this.colour});
+  const ReusableCard({
+    super.key,
+    required this.colour,
+    this.cardChild,
+  });
 
   final Color colour;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +90,26 @@ class ReusableCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         color: colour,
       ),
+      child: cardChild,
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  const IconContent({super.key, required this.icon, required this.text});
+
+  final FaIconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(height: 15.0),
+        FaIcon(icon, size: 80.0),
+        Text(text, style: TextStyle(fontSize: 18.0, color: Colors.white)),
+      ],
     );
   }
 }
