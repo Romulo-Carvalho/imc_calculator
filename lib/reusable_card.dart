2adc695a6ba0bf1aebb0732cd227cc6202db160b
Romulo <carvalhoreisr@gmail.com>
2026-04-15 21:51:29 -0300

    refactor: extract GestureDetector to ReusableCard using VoidCallback

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -5,20 +5,25 @@ class ReusableCard extends StatelessWidget {
     super.key,
     required this.colour,
     this.cardChild,
+    this.onPress,
   });
 
   final Color colour;
   final Widget? cardChild;
+  final VoidCallback? onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: colour,
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
